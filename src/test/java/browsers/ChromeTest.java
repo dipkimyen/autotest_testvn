f863bd2e71e8b9b6a78fe14392f944a1cefdfd64
dipkimyen <dipkimyen391@gmail.com>
2025-08-16 22:30:30 +0700

    add mobile view port tesst case

--- src/test/java/browsers/ChromeTest.java
+++ src/test/java/browsers/ChromeTest.java
@@ -6,21 +6,24 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChromeTest {
     // Test normal
-//    @Test
-//    public void normalModeTest(){
-//        //Open browser
-//        WebDriver driver = new ChromeDriver();
-//        //Navigate to website
-//        driver.get("https://www.selenium.dev/");
-//        //Verification
-//        Assert.assertEquals(driver.getTitle(),"Selenium");
-//        //Close browser
-//        driver.quit();
-//    }
+    @Test
+    public void normalModeTest(){
+        //Open browser
+        WebDriver driver = new ChromeDriver();
+        //Navigate to website
+        driver.get("https://www.selenium.dev/");
+        //Verification
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        //Close browser
+        driver.quit();
+    }
 
-    //Test run in background at github
+    //Test headless(Run in background at github)
     @Test
     public void headlessModeTest(){
         ChromeOptions chromeOptions = new ChromeOptions();
@@ -31,4 +34,22 @@ public class ChromeTest {
         Assert.assertEquals(driver.getTitle(),"Selenium");
         driver.quit();
     }
+
+    //Test mobile responsive
+    @Test
+    void mobileViewPortraitTesst(){
+        Map<String, Object> deviceMetrics = new HashMap<>();
+        deviceMetrics.put("width", 344);
+        deviceMetrics.put("height", 882);
+        Map<String, Object> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceMetrics", deviceMetrics);
+
+        ChromeOptions chromeOptions =  new ChromeOptions();
+        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+;    }
 }
