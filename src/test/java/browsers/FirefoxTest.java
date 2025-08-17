27cd0ac12b20bc9e6fb3bebf2fe25f59f0cbf820
dipkimyen <dipkimyen391@gmail.com>
2025-08-17 11:24:23 +0700

    add firefox test

--- src/test/java/browsers/FirefoxTest.java
+++ src/test/java/browsers/FirefoxTest.java
@@ -1,4 +1,30 @@
 package browsers;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class FirefoxTest {
+    //1. Default Mode
+    @Test
+    void openBrowserWithDefaultMode(){
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+
+    //2. Headless Mode
+    @Test
+    void openBrowserWithHeadlessMode(){
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--headless");
+
+        WebDriver driver = new FirefoxDriver(firefoxOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
 }
