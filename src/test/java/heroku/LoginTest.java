ca4edb634f87360f3b2e09079843c4f3032061c6
dipkimyen <dipkimyen391@gmail.com>
2025-08-17 16:54:27 +0700

    add test case login

---
+++ src/test/java/heroku/LoginTest.java
@@ -0,0 +1,28 @@
+package heroku;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    void successfullWithValidCredentials() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button[type=submit]")).click();
+        Thread.sleep(2000);
+        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/secure");
+
+        String successMessage = driver.findElement(By.className("success")).getText();
+        Assert.assertTrue(successMessage.contains("You logged into a secure area!"));
+
+        driver.quit();
+
+
+    }
+}
