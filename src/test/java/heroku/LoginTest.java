a5e803f55bfd0fd514f183d2b93455cfbb7c0eca
dipkimyen <dipkimyen391@gmail.com>
2025-09-13 09:07:49 +0700

    add changed

--- src/test/java/heroku/LoginTest.java
+++ src/test/java/heroku/LoginTest.java
@@ -6,23 +6,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.openqa.selenium.By.className;
+
 public class LoginTest {
     @Test
     void successfullWithValidCredentials() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/login");
+//        driver.get("https://the-internet.herokuapp.com/login");
+//
+//        driver.findElement(By.id("username")).sendKeys("tomsmith");
+//        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+//        driver.findElement(By.cssSelector("button[type=submit]")).click();
+//        Thread.sleep(2000);
+//        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/secure");
+//
+//        String successMessage = driver.findElement(By.className("success")).getText();
+//        Assert.assertTrue(successMessage.contains("You logged into a secure area!"));
+//
+//        driver.quit();
 
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.get("https://arobid.stg.arobid.site/vi/authentication/sign-in");
+
+        driver.findElement(By.cssSelector("input.mantine-Input-input.mantine-TextInput-input")).sendKeys("yennguyen1@yopmail.com");
+        driver.findElement(By.cssSelector("input.mantine-PasswordInput-innerInput")).sendKeys("Pass@123");
         driver.findElement(By.cssSelector("button[type=submit]")).click();
         Thread.sleep(2000);
-        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/secure");
-
-        String successMessage = driver.findElement(By.className("success")).getText();
-        Assert.assertTrue(successMessage.contains("You logged into a secure area!"));
+        Assert.assertEquals(driver.getCurrentUrl(), "https://arobid.stg.arobid.site/vi");
 
         driver.quit();
-
-
     }
 }
